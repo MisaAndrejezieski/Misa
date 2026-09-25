b7bf5eba4c1fc970f6d87cf722fcc6dd03c48cbe
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-09-25 17:59:05 -0300

    Update animar.js

--- animar.js
+++ animar.js
@@ -23,7 +23,7 @@ document.querySelectorAll('.menu-mobile nav a').forEach(link => {
     link.addEventListener('click', fecharMenu);
 });
 
-// SCROLL SUAVE PARA OS LINKS INTERNOS (só funciona em âncoras #)
+// SCROLL SUAVE PARA OS LINKS
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         const href = this.getAttribute('href');
